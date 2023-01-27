284fa78719ac590bafc2f444d1d85a0cd83463c0
BBerkeYildirim <batuhanberke.yildirim@gmail.com>
2023-01-28 00:02:13 +0300

    postgresql added and some production changes added

--- digitalmenu/settings.py
+++ digitalmenu/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'digitalmenu.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'menutech', 
+        'USER': 'postgres',
+        'PASSWORD': '12345',
+        'HOST': 'localhost', 
+        'PORT': '5432',
     }
 }
 
