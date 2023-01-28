d75de51f9287cae325188e73b000c4c24853f6c5
BBerkeYildirim <batuhanberke.yildirim@gmail.com>
2023-01-28 23:01:03 +0300

    project name changed

--- menutech/asgi.py
+++ menutech/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for digitalmenu project.
+ASGI config for menutech project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'digitalmenu.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'menutech.settings')
 
 application = get_asgi_application()
--- menutech/wsgi.py
+++ menutech/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for digitalmenu project.
+WSGI config for menutech project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'digitalmenu.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'menutech.settings')
 
 application = get_wsgi_application()
